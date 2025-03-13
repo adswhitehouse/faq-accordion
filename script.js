0d420f8c7468c408decb10ae5d3e0fadbaa736c5
Adam Whitehouse <adswhitehouse95@gamil.com>
2025-03-13 10:46:17 +0000

    added responsiveness for different screen sizes

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 let answers = document.querySelectorAll(".jsAnswer");
 let answerToggles = document.querySelectorAll(".jsAnswerToggle");
 
+// Clicking on the FAQ question div toggles the answer visibility and image
 answerToggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
-    toggle.parentElement.nextElementSibling.classList.toggle("show-answer");
-    toggle.src.includes("icon-plus.svg")
-      ? (toggle.src = "./images/icon-minus.svg")
-      : (toggle.src = "./images/icon-plus.svg");
+    toggle.nextElementSibling.classList.toggle("show-answer");
+    let icon = toggle.firstElementChild.nextElementSibling;
+    icon.src.includes("icon-plus.svg")
+      ? (icon.src = "./images/icon-minus.svg")
+      : (icon.src = "./images/icon-plus.svg");
   });
 });
