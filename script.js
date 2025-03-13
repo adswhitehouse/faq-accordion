a1bd0d84e1d261c5a85e260a03294d58f63af9fe
Adam Whitehouse <adswhitehouse95@gamil.com>
2025-03-13 11:25:13 +0000

    minor code alterations

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Variables
-let answers = document.querySelectorAll(".jsAnswer");
-let answerToggles = document.querySelectorAll(".jsAnswerToggle");
+const answers = document.querySelectorAll(".jsAnswer");
+const answerToggles = document.querySelectorAll(".jsAnswerToggle");
 
 // Clicking on the FAQ question div toggles the answer visibility and image
 answerToggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggle.nextElementSibling.classList.toggle("show-answer");
-    let icon = toggle.firstElementChild.nextElementSibling;
+    const icon = toggle.querySelector("img");
     icon.src.includes("icon-plus.svg")
       ? (icon.src = "./images/icon-minus.svg")
       : (icon.src = "./images/icon-plus.svg");
